816c518123b2add539ac73aa7b538fde78f846ce
idouaddi-ayoub <113615894+idouaddi-ayoub@users.noreply.github.com>
2024-09-15 02:39:48 +0100

    SVG layer

--- src/app/layout/Navbar.tsx
+++ src/app/layout/Navbar.tsx
@@ -19,12 +19,44 @@ const svgPath = {
   },
 };
 
+const svgFill = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      delay: 2,
+    },
+  },
+};
+
+const svgIdle = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      delay: 1,
+    },
+  },
+};
+
 const Navbar = () => {
   const navlink = [
     { href: "/", label: "Home" },
     { href: "/about-us", label: "About Us" },
-    { href: "/", label: "Test" },
-    { href: "/", label: "Bruh" },
+    { href: "/", label: "Events" },
+    { href: "/", label: "Appah" },
   ];
   const [activeNav, setActiveNav] = useState(navlink[0].label);
 
@@ -68,61 +100,121 @@ const Navbar = () => {
           <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
-            width={80}
-            height={80}
+            width={90}
+            height={90}
             initial="hidden"
             animate="visible"
           >
             <defs>
-              <style>{".cls-1{fill:#fff;}"}</style>
+              <style>
+                {".strokefill{stroke:green;}"}
+                {".fill{fill:white;}"}
+              </style>
             </defs>
-            <g id="Layer_2" data-name="Layer 2">
-              <g id="Layer_1-2" data-name="Layer 1">
+            {/* fill */}
+            <g id="Layer_1" data-name="Layer 1">
+              {/* C */}
+              <motion.path
+                className="fill"
+                d="M62.5 20 H34 L10 68.5 L67 68.5 L71 60 L25 60 L39.5 29 L59 29Z"
+                animate="visible"
+                initial="hidden"
+                variants={svgFill}
+                strokeWidth={0.5}
+                stroke="green"
+              />
+              {/* D */}
+              <motion.path
+                className="fill"
+                d="M75 20 H66.6 L61 33 L41.5 33 L30.5 57 L59 57Z"
+                animate="visible"
+                initial="hidden"
+                variants={svgFill}
+                strokeWidth={0.5}
+                stroke="green"
+              />
+              {/* ° */}
+              <motion.path
+                fill="#0D0D0D"
+                d="M57.9 40 H47.9 L43.5 49 L53.5 49Z"
+                animate="visible"
+                initial="hidden"
+                variants={svgIdle}
+                strokeWidth={0.5}
+                stroke="green"
+              />
+              {/* . */}
+              <motion.path
+                className="fill"
+                d="M90 20 H80 L75.5 29 L85.5 29Z"
+                animate="visible"
+                initial="hidden"
+                variants={svgFill}
+                strokeWidth={0.5}
+                stroke="green"
+              />
+              {/* I */}
+              <motion.path
+                className="fill"
+                d="M83.25 32.75 H73.5 L62.25 57 L72.25 57Z"
+                animate="visible"
+                initial="hidden"
+                variants={svgFill}
+                strokeWidth={0.5}
+                stroke="green"
+              />
+              {/* stroke */}
+              <g id="Layer_2" data-name="Layer 2">
+                {/* C */}
                 <motion.path
+                  className="strokefill"
                   fill="none"
                   d="M62.5 20 H34 L10 68.5 L67 68.5 L71 60 L25 60 L39.5 29 L59 29Z"
                   animate="visible"
                   initial="hidden"
                   variants={svgPath}
                   strokeWidth={0.5}
-                  stroke="white"
                 />
+
+                {/* D */}
                 <motion.path
-                  fill="none"
-                  d="M57.9 40 H47.9 L43.5 49 L53.5 49Z"
-                  animate="visible"
-                  initial="hidden"
-                  variants={svgPath}
-                  strokeWidth={0.5}
-                  stroke="white"
-                />
-                <motion.path
+                  className="strokefill"
                   fill="none"
                   d="M75 20 H66.6 L61 33 L41.5 33 L30.5 57 L59 57Z"
                   animate="visible"
                   initial="hidden"
                   variants={svgPath}
                   strokeWidth={0.5}
-                  stroke="white"
                 />
-
+                {/* ° */}
                 <motion.path
+                  className="strokefill"
+                  fill="none"
+                  d="M57.9 40 H47.9 L43.5 49 L53.5 49Z"
+                  animate="visible"
+                  initial="hidden"
+                  variants={svgPath}
+                  strokeWidth={0.5}
+                />
+                {/* . */}
+                <motion.path
+                  className="strokefill"
                   fill="none"
                   d="M90 20 H80 L75.5 29 L85.5 29Z"
                   animate="visible"
                   initial="hidden"
                   variants={svgPath}
                   strokeWidth={0.5}
-                  stroke="white"
                 />
+                {/* I */}
                 <motion.path
+                  className="strokefill"
                   fill="none"
                   d="M83.25 32.75 H73.5 L62.25 57 L72.25 57Z"
                   animate="visible"
                   initial="hidden"
                   variants={svgPath}
                   strokeWidth={0.5}
-                  stroke="white"
                 />
               </g>
             </g>
